ba56c33dad49946bf6bc77605940436d5299e412
zhaolu <1632089314@qq.com>
2016-06-10 13:55:44 +0800

    finished

--- src/separators.js
+++ src/separators.js
@@ -3,26 +3,40 @@
 function thousands_separators(number) {
 
     var number = number.toString();
-    var result = '';
 
     if (number.indexOf('.') === -1) {
-        while (number.length > 3) {
-            result += ',' + number.slice(-3);
-            number = number.slice(0, number.length - 3);
-        }
-        if (number) {
-            result = number + result;
-        }
+        return numberHasNotPoint(number);
     } else {
-        var array = number.split('.');
+        return numberHasPoint(number);
+    }
+}
 
-        while (array[0].length > 3) {
-            result += ',' + array[0].slice(-3);
-            array[0] = array[0].slice(0, array[0].length - 3);
-        }
-        if (array[0]) {
-            result = array[0] + result + '.' + array[1];
-        }
+function numberHasNotPoint(number) {
+
+    var result = '';
+
+    while (number.length > 3) {
+        result += ',' + number.slice(-3);
+        number = number.slice(0, number.length - 3);
+    }
+    if (number) {
+        result = number + result;
+    }
+
+    return result;
+}
+
+function numberHasPoint(number) {
+
+    var array = number.split('.');
+    var result = '';
+
+    while (array[0].length > 3) {
+        result += ',' + array[0].slice(-3);
+        array[0] = array[0].slice(0, array[0].length - 3);
+    }
+    if (array[0]) {
+        result = array[0] + result + '.' + array[1];
     }
 
     return result;
